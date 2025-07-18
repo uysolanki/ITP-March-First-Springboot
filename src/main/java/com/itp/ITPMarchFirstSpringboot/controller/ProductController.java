08499e04a71591dd698ad64a3530c3fbc63d6734
uysolanki <uysolanki@gmail.com>
2025-07-18 15:29:07 +0530

    initial commit

--- src/main/java/com/itp/ITPMarchFirstSpringboot/controller/ProductController.java
+++ src/main/java/com/itp/ITPMarchFirstSpringboot/controller/ProductController.java
@@ -1,8 +1,13 @@
 package com.itp.ITPMarchFirstSpringboot.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itp.ITPMarchFirstSpringboot.entity.Product;
 import com.itp.ITPMarchFirstSpringboot.service.ProductService;
 
 @RestController
@@ -10,5 +15,11 @@ public class ProductController {
 	
 	@Autowired
 	ProductService productService;
+	
+	@PostMapping("/loadMultipleProducts")
+	public List<Product> loadMultipleProducts(@RequestBody List<Product> products)
+	{
+		return productService.loadMultipleProducts(products);
+	}
 
 }
--- src/main/java/com/itp/ITPMarchFirstSpringboot/service/ProductService.java
+++ src/main/java/com/itp/ITPMarchFirstSpringboot/service/ProductService.java
@@ -1,8 +1,11 @@
 package com.itp.ITPMarchFirstSpringboot.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.itp.ITPMarchFirstSpringboot.entity.Product;
 import com.itp.ITPMarchFirstSpringboot.repository.ProductRepository;
 
 @Service
@@ -10,5 +13,9 @@ public class ProductService {
 	
 	@Autowired
 	ProductRepository productRepository;
+
+	public List<Product> loadMultipleProducts(List<Product> products) {
+		return productRepository.saveAll(products);
+	}
 
 }
